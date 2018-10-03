e27ea7d01ff4e67daccc5726cdaff95c194ffeba
Jonas Bernoulli <jonas@bernoul.li>
2018-10-04 01:14:12 +0200

    Don't advice select-frame and delete-frame

--- moody.el
+++ moody.el
@@ -297,8 +297,6 @@ to the command loop."
 (add-hook 'window-configuration-change-hook 'moody--set-active-window)
 (add-hook 'focus-in-hook                    'moody--set-active-window)
 (advice-add 'select-window :after           'moody--set-active-window)
-(advice-add 'select-frame :after            'moody--set-active-window)
-(advice-add 'delete-frame :after            'moody--set-active-window)
 (advice-add 'handle-switch-frame :after     'moody--set-active-window)
 
 (defun moody--unset-active-window (&rest _)
