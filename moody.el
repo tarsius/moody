f5114818883b9482e8103574875ac276e905de0f
Jonas Bernoulli <jonas@bernoul.li>
2024-01-01 23:21:18 +0100

    Bump copyright years

--- moody.el
+++ moody.el
@@ -1,6 +1,6 @@
 ;;; moody.el --- Tabs and ribbons for the mode line  -*- lexical-binding:t -*-
 
-;; Copyright (C) 2018-2023 Jonas Bernoulli
+;; Copyright (C) 2018-2024 Jonas Bernoulli
 
 ;; Author: Jonas Bernoulli <jonas@bernoul.li>
 ;; Homepage: https://github.com/tarsius/moody
