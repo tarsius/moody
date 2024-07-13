dc212d034d49935bb1b2391996896a0237443222
Jonas Bernoulli <jonas@bernoul.li>
2024-07-13 21:26:44 +0200

    Bump compat dependency

--- moody.el
+++ moody.el
@@ -6,7 +6,7 @@
 ;; Homepage: https://github.com/tarsius/moody
 ;; Keywords: faces
 
-;; Package-Requires: ((emacs "25.3") (compat "29.1.4.5"))
+;; Package-Requires: ((emacs "25.3") (compat "30.0.0.0"))
 
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 
