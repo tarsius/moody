698376065e78201577b371ccd9a340b24cd59331
Jonas Bernoulli <jonas@bernoul.li>
2025-09-01 18:12:24 +0200

    Release version 1.1.5

--- moody.el
+++ moody.el
@@ -6,7 +6,7 @@
 ;; Homepage: https://github.com/tarsius/moody
 ;; Keywords: faces
 
-;; Package-Version: 1.1.4
+;; Package-Version: 1.1.5
 ;; Package-Requires: ((emacs "26.1") (compat "30.1"))
 
 ;; SPDX-License-Identifier: GPL-3.0-or-later
